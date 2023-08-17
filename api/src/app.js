317f65102bf23ecfd1cddf12f260a2957a205f64
juannpz <juannicolaspuchetazavala@gmail.com>
2023-08-17 18:24:03 -0300

    origins added

--- api/src/app.js
+++ api/src/app.js
@@ -17,11 +17,22 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use(cors({
-  origin: 'https://pi-pokemon-omega-rust.vercel.app/',
+  origin: '*',
   credentials: true,
   methods: 'GET, POST, OPTIONS, PUT, DELETE',
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
 }));
+
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // server.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
