9c203d5f1a6c844b3bb2c654e1b583da3a8380d2
juannpz <juannicolaspuchetazavala@gmail.com>
2023-08-17 18:30:11 -0300

    origins added

--- api/src/app.js
+++ api/src/app.js
@@ -16,31 +16,26 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 
-server.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: 'GET, POST, OPTIONS, PUT, DELETE',
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-}));
+// este es el mio
+// server.use(cors({
+//   origin: '*',
+//   credentials: true,
+//   methods: 'GET, POST, OPTIONS, PUT, DELETE',
+//   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+// }));
 
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
-
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+const Options = { origin: "" }; // Habilito las CORS en la aplicacion
+server.use(cors(Options));
 
 server.use('/', routes);
 
